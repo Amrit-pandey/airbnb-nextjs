8126b2f71a20dea6b34a890432db624c900492a5
Amrit <amritpandey2005@gmail.com>
2024-01-19 14:49:01 +0530

    fixing bug

--- components/inputs/Calender.tsx
+++ components/inputs/Calender.tsx
@@ -1,16 +1,12 @@
-'use client';
+"use client";
 
-import { 
-  DateRange, 
-  Range, 
-  RangeKeyDict
-} from 'react-date-range';
+import { DateRange, Range, RangeKeyDict } from "react-date-range";
 
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 
 interface DatePickerProps {
-  value: Range,
+  value: Range;
   onChange: (value: RangeKeyDict) => void;
   disabledDates?: Date[];
 }
@@ -18,12 +14,11 @@ interface DatePickerProps {
 const DatePicker: React.FC<DatePickerProps> = ({
   value,
   onChange,
-  disabledDates
+  disabledDates,
 }) => {
-  console.log("Ranges", Range)
-  return ( 
+  return (
     <DateRange
-      rangeColors={['#262626']}
+      rangeColors={["#262626"]}
       ranges={[value]}
       date={new Date()}
       onChange={onChange}
@@ -32,7 +27,7 @@ const DatePicker: React.FC<DatePickerProps> = ({
       minDate={new Date()}
       disabledDates={disabledDates}
     />
-   );
-}
- 
+  );
+};
+
 export default DatePicker;
